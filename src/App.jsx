8ed8df2eb6f333460a5154475ce67caec9995847
NishadHossainEmon <nishad.hossain.emon.77@gmail.com>
2026-04-01 12:09:48 +0600

    h1 tag added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ function App() {
   return (
     <>
        <h1 className='bg-red-600 text-center text-2xl'>Project Build Started</h1>
+       <h1 className="text-center text-5xl">Helo hello hello.....</h1>
     </>
   )
 }
